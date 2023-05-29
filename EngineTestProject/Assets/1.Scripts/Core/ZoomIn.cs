a559b48168529328a46fb20a061efe5bb4e32cee
CAFUCH <tjfdk6201@gmail.com>
2023-05-29 23:42:07 +0900

    ZoomIn
    zoomin 개발 완료

--- EngineTestProject/Assets/1.Scripts/Core/ZoomIn.cs
+++ EngineTestProject/Assets/1.Scripts/Core/ZoomIn.cs
@@ -1,38 +1,52 @@
-    using Microsoft.Win32.SafeHandles;
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using DG.Tweening;
+using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
 
-    public class ZoomIn : MonoBehaviour
-    {
+public class ZoomIn : MonoBehaviour
+{
     [SerializeField] private Camera _cam;
     [SerializeField] private GameObject _map;
     [SerializeField] private Ease ease;
 
     [SerializeField] float minZoom ,maxZoom;
     [SerializeField] float minHeight, maxHeight;
-        float dt;
 
-        private void Start() {
-            
-            
-        }
+    bool isZoom = false;
+    float dt;
 
-        private void Update() { //다트윈 쓹
-            
-            if (Input.GetKeyDown(KeyCode.Mouse1)) {
+    private void Start() {
+        
+        // Tweener twn = DOTween.To(() => 
+        // minZoom, value => dt = value, maxZoom, 1.5f).SetEase(ease); //문제 있음
 
-                Debug.Log("click");
+        
+    }
 
-                // Tweener twn = DOTween.To(() => 
-                // minZoom, value => dt = value, maxZoom, 1.5f).SetEase(ease); //문제 있음
+    private void Update() { //다트윈 쓹
+        
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
 
-                // if (_cam.fieldOfView >= maxZoom)
-                //     _cam.fieldOfView = _cam.transform.DOMoveX
+            isZoom = !isZoom;
+
+            if (isZoom) { // zoom true 라면
+
+                DOVirtual.Float(minZoom, maxZoom, 1f, value => {
+                    _cam.fieldOfView = value; }).SetEase(ease);
 
                 if (_map.transform.position.y >= maxHeight)
-                    _map.transform.DOMoveY(maxHeight, 1.5f).SetEase(ease);
+                    _map.transform.DOMoveY(maxHeight, 1f).SetEase(ease);
+            }
+            else {
+
+                DOVirtual.Float(maxZoom, minZoom, 1f, value => {
+                    _cam.fieldOfView = value; }).SetEase(ease);
+
+                if (_map.transform.position.y <= maxHeight)
+                    _map.transform.DOMoveY(minHeight, 1f).SetEase(ease);
             }
         }
+
     }
+}
