98fb586bb1d0c9ab18958ff2e8d271cae796088e
CAFUCH <tjfdk6201@gmail.com>
2023-05-29 13:28:03 +0900

    zoomint ainmation
    dotween 사용
    zoomin 개발중

--- EngineTestProject/Assets/1.Scripts/Core/ZoomIn.cs
+++ EngineTestProject/Assets/1.Scripts/Core/ZoomIn.cs
@@ -1,23 +1,38 @@
-using Microsoft.Win32.SafeHandles;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using Microsoft.Win32.SafeHandles;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using DG.Tweening;
 
-public class ZoomIn : MonoBehaviour
-{
-   [SerializeField] private Camera _cam;
-   [SerializeField] private GameObject _map;
+    public class ZoomIn : MonoBehaviour
+    {
+    [SerializeField] private Camera _cam;
+    [SerializeField] private GameObject _map;
+    [SerializeField] private Ease ease;
 
-   [SerializeField] float minZoom ,maxZoom;
-   [SerializeField] float minHeight, maxHeight;
+    [SerializeField] float minZoom ,maxZoom;
+    [SerializeField] float minHeight, maxHeight;
+        float dt;
 
-    private void Update() { //다트윈 쓹
-        
-        // if (Input.GetKeyDown(KeyCode.Mouse1)) {
+        private void Start() {
+            
+            
+        }
 
-        //     if (_cam.fieldOfView <= maxZoom)
-        //         _cam.fieldOfView += Time.deltaTime;
-        //     if (_map.transform.position.y <= maxHeight)
-        // }
+        private void Update() { //다트윈 쓹
+            
+            if (Input.GetKeyDown(KeyCode.Mouse1)) {
+
+                Debug.Log("click");
+
+                // Tweener twn = DOTween.To(() => 
+                // minZoom, value => dt = value, maxZoom, 1.5f).SetEase(ease); //문제 있음
+
+                // if (_cam.fieldOfView >= maxZoom)
+                //     _cam.fieldOfView = _cam.transform.DOMoveX
+
+                if (_map.transform.position.y >= maxHeight)
+                    _map.transform.DOMoveY(maxHeight, 1.5f).SetEase(ease);
+            }
+        }
     }
-}
